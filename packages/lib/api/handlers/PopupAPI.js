b95429651bb8ed4454ad889de107113360377302
hoangnv1203 <hoang.nong.nv@gmail.com>
2019-07-08 17:24:33 +0700

    Update api lib

---
+++ packages/lib/api/handlers/PopupAPI.js
@@ -0,0 +1,85 @@
+import { setRecommendDappList } from '@ezpay/popup/src/reducers/appReducer';
+
+export default {
+    init(duplex) {
+        this.duplex = duplex;
+    },
+
+    //Data refresh
+    refresh() {
+        return this.duplex.send('refresh');
+    },
+    // Data requesting
+
+    requestState() {
+        return this.duplex.send('requestState');
+    },
+
+    changeState(appState) {
+        return this.duplex.send('changeState', appState, false);
+    },
+
+    resetState() {
+        return this.duplex.send('resetState', {}, false);
+    },
+
+    getPrices() {
+        return this.duplex.send('getPrices');
+    },
+
+    getConfirmations() {
+        return this.duplex.send('getConfirmations');
+    },
+
+    // Confirmation actions
+
+    acceptConfirmation(whitelistDuration) {
+        return this.duplex.send('acceptConfirmation', whitelistDuration, false);
+    },
+
+    rejectConfirmation() {
+        return this.duplex.send('rejectConfirmation', {}, false);
+    },
+
+    // Account control
+
+    importAccount(privateKey, name) {
+        return this.duplex.send('importAccount', { privateKey, name });
+    },
+
+    addAccount(mnemonic, name) {
+        return this.duplex.send('addAccount', { mnemonic, name });
+    },
+
+    selectAccount(address) {
+        this.duplex.send('selectAccount', address, false);
+    },
+
+    deleteAccount() {
+        this.duplex.send('deleteAccount', {}, false);
+    },
+
+    getAccounts() {
+        return this.duplex.send('getAccounts');
+    },
+
+    exportAccount() {
+        return this.duplex.send('exportAccount');
+    },
+
+    getSelectedAccount() {
+        return this.duplex.send('getSelectedAccount');
+    },
+
+    getAccountDetails(address) {
+        return this.duplex.send('getAccountDetails', address);
+    },
+
+    getSetting() {
+        return this.duplex.send('getSetting');
+    },
+
+    setSetting(setting) {
+        this.duplex.send('setSetting', setting, false);
+    }
+}
--- packages/popup/src/index.js
+++ packages/popup/src/index.js
@@ -12,6 +12,8 @@ import { configureStore, getDefaultMiddleware } from 'redux-starter-kit';
 import { setConfirmations } from 'reducers/confirmationsReducer';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { version } from '@ezpay/popup/package';
+import { PopupAPI } from '@ezpay/lib/api';
+import MessageDuplex from '@ezpay/lib/MessageDuplex';
 
 import {
     setAppState,
@@ -45,15 +47,17 @@ import {
 addLocaleData([...en]);
 Sentry.init({
     dsn: 'https://a52a6098294d4c1c8397e22c8b9a1c0f@sentry.io/1455110',
-    release: `TronLink@${ process.env.REACT_APP_VERSION }`
+    release: `EZpay@${ process.env.REACT_APP_VERSION }`
 });
 
 const logger = new Logger('Popup');
 
 export const app = {
+    duplex: new MessageDuplex.Popup(),
     async run() {
         this.createStore();
-        // await this.getAppState();
+        await this.getAppState();
+        this.bindDuplexRequests();
         this.render();
     },
 
@@ -69,6 +73,99 @@ export const app = {
         });
 
         logger.info('Created store', this.store);
+    },
+
+    async getAppState() {
+        PopupAPI.init(this.duplex);
+        const setting = await PopupAPI.getSetting();
+
+        // let [
+        //     appState,
+        //     // accounts,
+        //     selectedAccount,
+        //     language,
+        // ] = await Promise.all([
+        //     PopupAPI.requestState(),
+        //     // PopupAPI.getAccounts(),
+        //     PopupAPI.getSelectedAccount(),
+        //     PopupAPI.getLanguage()
+        // ]);
+        // const lang = navigator.language || navigator.browserLanguage;
+        // if ( lang.indexOf('zh') > -1 ) {
+        //     language = language || 'zh';
+        // } else if ( lang.indexOf('ja') > -1 ) {
+        //     language = language || 'ja';
+        // } else {
+        //     language = language || 'en';
+        // }
+        // this.store.dispatch(setAppState(appState));
+        // // this.store.dispatch(setAccounts(accounts));
+        // // this.store.dispatch(setCurrency(prices.selected));
+        // this.store.dispatch(setLanguage(language));
+        // this.store.dispatch(setSetting(setting));
+        // if(selectedAccount)
+        //     this.store.dispatch(setAccount(selectedAccount));
+
+        logger.info('Set application state');
+    },
+
+    bindDuplexRequests() {
+        this.duplex.on('setState', appState => this.store.dispatch(
+            setAppState(appState)
+        ));
+
+        this.duplex.on('setConfirmations', confirmations => this.store.dispatch(
+            setConfirmations(confirmations)
+        ));
+
+        this.duplex.on('setAccount', account => this.store.dispatch(
+            setAccount(account)
+        ));
+
+        this.duplex.on('setAccounts', accounts => this.store.dispatch(
+            setAccounts(accounts)
+        ));
+
+        this.duplex.on('setPriceList', priceList => this.store.dispatch(
+            setPriceList(priceList)
+        ));
+
+        this.duplex.on('setCurrency', currency => this.store.dispatch(
+            setCurrency(currency)
+        ));
+
+        this.duplex.on('setSelectedToken', token => this.store.dispatch(
+            setToken(token)
+        ));
+
+        this.duplex.on('setLanguage', language => this.store.dispatch(
+            setLanguage(language)
+        ));
+
+        this.duplex.on('setSetting', setting => this.store.dispatch(
+            setSetting(setting)
+        ));
+
+        this.duplex.on('setSelectedBankRecordId', id => this.store.dispatch(
+            setSelectedBankRecordId(id)
+        ));
+
+        this.duplex.on('changeDealCurrencyPage', status => this.store.dispatch(
+            changeDealCurrencyPage(status)
+        ));
+
+        this.duplex.on('setAirdropInfo', airdropInfo => this.store.dispatch(
+            setAirdropInfo(airdropInfo)
+        ));
+
+        this.duplex.on('setDappList', dappList => this.store.dispatch(
+            setDappList(dappList)
+        ));
+
+        this.duplex.on('setAuthorizeDapps', authorizeDapps => this.store.dispatch(
+            setAuthorizeDapps(authorizeDapps)
+        ));
+
     },
 
     render() {
