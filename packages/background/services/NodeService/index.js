2dacc7159751a21c3b7c9281cd662cde4ffc5fe1
hoangnv1203 <hoang.nong.nv@gmail.com>
2019-07-10 15:55:05 +0700

    Add default node

--- packages/background/services/NodeService/index.js
+++ packages/background/services/NodeService/index.js
@@ -5,22 +5,46 @@ const NodeService = {
         'f0b1e38e-7bee-485e-9d3f-69410bf30681': {
             type: 'TRON',
             name: 'Mainnet',
-            fullNode: 'https://api.trongrid.io',
-            solidityNode: 'https://api.trongrid.io',
-            eventServer: 'https://api.trongrid.io',
+            decimal: 6,
+            logo: '',
+            apiUrl: '',
+            apiKey: '',
+            endPoint: 'https://api.trongrid.io',
             default: true // false
         },
         '6739be94-ee43-46af-9a62-690cf0947269': {
             type: 'TRON',
             name: 'Shasta Testnet',
-            fullNode: 'https://api.shasta.trongrid.io',
-            solidityNode: 'https://api.shasta.trongrid.io',
-            eventServer: 'https://api.shasta.trongrid.io',
+            decimal: 6,
+            logo: '',
+            apiUrl: '',
+            apiKey: '',
+            endPoint: 'https://api.shasta.trongrid.io',
+            default: true
+        },
+        '6739be94-ee43-46af-9a62-690cf0947280': {
+            type: 'ETH',
+            name: 'Ethereum',
+            decimal: 18,
+            logo: '',
+            apiUrl: '',
+            apiKey: 'BGEU4V19ZPHVJSHUM3UDFXZ9BSBQM2FM88',
+            endPoint: 'https://mainnet.infura.io/v3/9a150d3a322645268224160ebf5b8599',
+            default: true
+        },
+        '6739be94-ee43-46af-9a62-690cf0947281': {
+            type: 'ETH_RINKEBY',
+            name: 'Rinkeby',
+            decimal: 18,
+            logo: '',
+            apiUrl: '',
+            apiKey: 'BGEU4V19ZPHVJSHUM3UDFXZ9BSBQM2FM88',
+            endPoint: 'https://rinkeby.infura.io/v3/9a150d3a322645268224160ebf5b8599',
             default: true
         }
     },
 
-    _selectedNode: 'f0b1e38e-7bee-485e-9d3f-69410bf30681',
+    _selectedNode: '6739be94-ee43-46af-9a62-690cf0947281',
 
     _read() {
         const {
--- packages/background/services/WalletService/Chain.js
+++ packages/background/services/WalletService/Chain.js
@@ -3,7 +3,7 @@ import Utils from '@tronlink/lib/utils';
 class Chain {
     constructor(type, endPoint, decimal, apiUrl, apiKey) {
         this.type = type;
-        this.endpoint = endPoint;
+        this.endPoint = endPoint;
         this.decimal = decimal;
         this.apiUrl = apiUrl;
         this.apiKey = apiKey;
